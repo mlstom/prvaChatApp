b2312dcd3a69d12bbf0fd737a07f16117785e291
Milos Tomic <milostomic502@gmail.com>
2022-09-13 06:37:22 +0200

    radidi

---
+++ pages/api/socketio.js
@@ -0,0 +1,23 @@
+import { Server } from "socket.io"
+
+export default function handlerSocketimport(req, res) {
+    if (res.socket.server.io) {
+        console.log('Socket is already running')
+    } else {
+        console.log('*First runnig, Socket is initializing')
+        const io = new Server(res.socket.server)
+        
+        io.on('connection', socket => {
+            socket.on('newMessage',msgs=>{
+                socket.broadcast.emit('updatedMessages',msgs)
+            })
+        })
+        res.socket.server.io = io
+    }
+    res.end()
+}
+export const config = {
+    api: {
+        bodyParser: false
+    }
+}
